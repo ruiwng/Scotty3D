dacd0f76932e46c2ac584e1272c6b7a2b7f4cc7b
rain.wr <rain.wr@alibaba-inc.com>
2022-04-24 15:03:50 +0800

    feat: generate camera rays

--- src/student/camera.cpp
+++ src/student/camera.cpp
@@ -10,6 +10,8 @@ Ray Camera::generate_ray(Vec2 screen_coord) const {
     // canonical sensor plane one unit away from the pinhole.
     // Tip: Compute the ray direction in camera space and use
     // the camera transform to transform it back into world space.
-
-    return Ray();
+    float half_height = std::tanf(Radians(get_fov()) * 0.5);
+    float half_width = std::tanf(Radians(get_h_fov()) * 0.5);
+    Vec3 view_pos(half_width * (screen_coord.x - 0.5) * 2.0, half_height * (screen_coord.y - 0.5) * 2.0, -1.f);
+    return Ray(position, iview * view_pos - position);
 }
